4dfe75755afd2d1c9421accae0bb60a4e7f08fc2
ysaini4 <ysaini75@gmail.com>
2019-04-05 16:37:08 +0530

    Updates

---
+++ precache-manifest.4f329f264a0cd16e5fb706f60dd177a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a17434973ee99c41fd8",
+    "url": "/rent-care/static/js/runtime~main.f9d48b3e.js"
+  },
+  {
+    "revision": "b1012613d76fb8c0e0fc",
+    "url": "/rent-care/static/js/main.b2afbc88.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/rent-care/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b1012613d76fb8c0e0fc",
+    "url": "/rent-care/static/css/main.5c2205be.chunk.css"
+  },
+  {
+    "revision": "973014a45283dcf4c06e07363097cab6",
+    "url": "/rent-care/index.html"
+  }
+];
